b41987b86f7252c3c522ac54301d31aad72420c6
k-taru----- <sheva.7.krslqkr@gmail.com>
2021-03-18 17:08:23 +0900

    feat: return immediately in plugin

--- traefik_plugin.go
+++ traefik_plugin.go
@@ -1,65 +1,39 @@
 package plugin
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"net/http"
-	"text/template"
 )
 
 // Config the plugin configuration.
 type Config struct {
-	Headers map[string]string `json:"headers,omitempty"`
 }
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
-	return &Config{
-		Headers: make(map[string]string),
-	}
+	return &Config{}
 }
 
 type Demo struct {
-	next     http.Handler
-	headers  map[string]string
-	name     string
-	template *template.Template
+	next    http.Handler
+	headers map[string]string
+	name    string
 }
 
 func New(ctx context.Context, next http.Handler, config Config, name string) (*Demo, error) {
-	if len(config.Headers) == 0 {
-		return nil, fmt.Errorf("headers cannot be empty")
-	}
-
 	return &Demo{
-		headers:  config.Headers,
-		next:     next,
-		name:     name,
-		template: template.New("demo").Delims("[[", "]]"),
+		next: next,
+		name: name,
 	}, nil
 }
 
 func (a *Demo) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	fmt.Println("plugin served")
 
-	for key, value := range a.headers {
-		tmpl, err := a.template.Parse(value)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	rw.WriteHeader(http.StatusOK)
+	rw.Header().Add("x-test", "a.name")
+	return
 
-		writer := &bytes.Buffer{}
-
-		err = tmpl.Execute(writer, req)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		req.Header.Set(key, writer.String())
-	}
-
-	a.next.ServeHTTP(rw, req)
+	// a.next.ServeHTTP(rw, req)
 }
